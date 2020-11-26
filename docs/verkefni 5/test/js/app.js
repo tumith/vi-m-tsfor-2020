e0b2f74663cb05b290ec70bed8a7336614860b84
tumith <tumithorberg@gmail.com>
2020-11-26 14:30:21 +0000

    26.11.2020

--- docs/verkefni 5/test/js/app.js
+++ docs/verkefni 5/test/js/app.js
@@ -1,20 +1,50 @@
-firstPath = "M151 70C151 108.66 117.197 140 75.5 140C33.8025 140 0 108.66 0 70C0 31.3401 33.8025 0 75.5 0C117.197 0 151 31.3401 151 70Z";
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
-secoundPath = "M68.5 63.5C68.5 98.5701 106.332 127 68.5 127C30.6685 127 0 98.5701 0 63.5C0 28.4299 30.6685 0 68.5 0C106.332 0 68.5 28.4299 68.5 63.5Z";
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+recognition.lang = 'en-US';
+
+let p = document.createElement('p');
+const words = document.querySelector('.words');
+words.appendChild(p);
+
+const firstPath = "M151 70C151 108.66 117.197 140 75.5 140C33.8025 140 0 108.66 0 70C0 31.3401 33.8025 0 75.5 0C117.197 0 151 31.3401 151 70Z";
+
+const secoundPath = "M68.5 63.5C68.5 98.5701 106.332 127 68.5 127C30.6685 127 0 98.5701 0 63.5C0 28.4299 30.6685 0 68.5 0C106.332 0 68.5 28.4299 68.5 63.5Z";
 
 const no_smile = document.querySelector('#no_smile')
 let toggle = false;
 
-no_smile.addEventListener('click', () =>{
-    const timeline = anime.timeline({
-        duration : 750,
-        easing : 'easeOutExpo'
-    });
+recognition.addEventListener('result', e =>{
+    const transcript = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join('');
 
-    timeline.add({
-        target: ".smile",
-        d : [
-            {value : secoundPath}
-        ]
-    })
+    const poopScript = transcript.replace(/poop|poo|shit|dump/gi, 'poop');
+    p.textContent = poopScript;
+    console.log(poopScript);
+    p.textContent = transcript;
+
+     if (e.results[0].isFinal) {
+       p = document.createElement('p');
+       words.appendChild(p);
+     }
+
+     if (transcript.includes('smile')){ 
+        const timeline = anime.timeline({
+            duration : 750,
+            easing : "easeOutExpo"
+        });
+        timeline.add({
+            targets : ".smile",
+            d : [
+                {value: secoundPath}
+            ]
+        });
+    }
 });
+console.log(p)
+recognition.addEventListener('end', recognition.start);
+
+recognition.start();
